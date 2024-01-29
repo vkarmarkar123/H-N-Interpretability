ec6a86dd7913093129e6ecc34b971c0b96f266b7
mohamedhassan <hasssan8690@icloud.com>
2024-01-29 10:14:05 -0800

    code for genrating FLIm features for model analysis

---
+++ Bex.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.spatial.distance import pdist
+import numpy as np
+import pandas as pd
+
+def bex(subset_df):
+    def compute_distance(row1, row2):
+        """ Compute Pair-wise Euclidean distance """
+        return np.sqrt(
+            (row1['WLI_x_coord'] - row2['WLI_x_coord']) ** 2 + (row1['WLI_y_coord'] - row2['WLI_y_coord']) ** 2)
+
+    def process_data(df):
+
+        if set(df['Label'].unique()) == {0, 1}:
+            # Create a DataFrame for each label
+            df_label_0 = df[df['Label'] == 0]
+            df_label_1 = df[df['Label'] == 1]
+
+            # Calculate distances and filter out rows with distance < 20 pixels
+            to_drop = []
+            for index_0, row_0 in df_label_0.iterrows():
+                for index_1, row_1 in df_label_1.iterrows():
+                    if compute_distance(row_0, row_1) < 20:
+                        to_drop.extend([index_0, index_1])
+
+            df = df.drop(to_drop).reset_index(drop=True)
+
+        return df
+
+    caseID = subset_df['Case']
+    cases = np.unique(caseID)
+    train_batch = []
+
+    for Case in cases:
+        case_batch = subset_df[subset_df['Case'] == Case]
+        Run_ID = case_batch['Run']
+        Runs = np.unique(Run_ID)
+
+        for Run in Runs:
+            run_batch = case_batch[case_batch['Run'] == Run]
+            processed_df = process_data(run_batch)
+
+    return processed_df
---
+++ Fast_BEX.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import cdist
+
+def optimized_bex(subset_df):
+    def process_data(df):
+        if set(df['Label'].unique()) == {0, 1}:
+            # Splitting the DataFrame based on Label
+            df_label_0 = df[df['Label'] == 0]
+            df_label_1 = df[df['Label'] == 1]
+
+            # Compute pairwise distances using cdist from scipy
+            distances = cdist(df_label_0[['WLI_x_coord', 'WLI_y_coord']], df_label_1[['WLI_x_coord', 'WLI_y_coord']], metric='euclidean')
+            close_pairs = np.where(distances < 20)
+
+            # Indexes to drop
+            to_drop_0 = df_label_0.iloc[close_pairs[0]].index
+            to_drop_1 = df_label_1.iloc[close_pairs[1]].index
+            to_drop = to_drop_0.union(to_drop_1)
+
+            df = df.drop(to_drop).reset_index(drop=True)
+        return df
+
+    processed_dfs = []
+    for Case, case_group in subset_df.groupby('Case'):
+        for Run, run_group in case_group.groupby('Run'):
+            processed_dfs.append(process_data(run_group))
+
+    return pd.concat(processed_dfs, ignore_index=True)
