b850858a92bd3359e77d6e648157e80fbe417c15
Vikram Karmarkar <vikrampk2003@gmail.com>
2024-03-08 01:02:06 -0800

    DOT file generation

--- FLIm_Preprocessing_For_Model_Analysis.py
+++ FLIm_Preprocessing_For_Model_Analysis.py
@@ -1,16 +1,22 @@
-
 import os
 import pandas as pd
 from Fast_BEX import optimized_bex
-df = pd.read_csv("E:\OneDrive - University of California, Davis\HN Model Development\Data\HN_P2C_20231207.csv")
+import xgboost as xgb
+import matplotlib.pyplot as plt
+import shap
+import pickle
+import tkinter as tk
+from tkinter import filedialog
+
+df = pd.read_csv("./CSVs and Output Files/HN_P2C_20231207.csv")
 cwd = os.getcwd()
-filename = 'Anatomic_DT_LG'
-output_dir = os.path.join(cwd, filename)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+# filename = 'Anatomic_DT_LG'
+# output_dir = os.path.join(cwd, filename)
+# if not os.path.exists(output_dir):
+#     os.makedirs(output_dir)
 df = df[df['ScanContext'] == 'In Vivo']
 
-# %% SNR and life time filtering
+# %% SNR and lifetime filtering
 index = df[((df['snr_ch1'] < 30) & (df['Instrument'] == 'V4')) |
            ((df['snr_ch2'] < 30) & (df['Instrument'] == 'V4')) |
            ((df['snr_ch3'] < 30) & (df['Instrument'] == 'V4'))].index
@@ -21,17 +27,14 @@ index = df[((df['snr_ch1'] < 50) & (df['Instrument'] == 'FLImBrush')) |
 df.drop(index, inplace=True)
 
 # R_data((R_data.lifet_avg_ch1 > 10 | R_data.lifet_avg_ch1 < 2),:) = [];
-
 index = df[((df['lifet_avg_ch1'] > 16) | (df['lifet_avg_ch1'] < 1)) |
            ((df['lifet_avg_ch2'] > 16) | (df['lifet_avg_ch2'] < 1)) |
            ((df['lifet_avg_ch3'] > 16) | (df['lifet_avg_ch3'] < 1))].index
 df.drop(index, inplace=True)
 
 # %% For our current analysis we remove all V4 data and no channel 4 data
-
 index = df[df['Instrument'] == 'FLImBrush'].index
 df.drop(index, inplace=True)
-
 
 # df = df[df['Anatomy'] =='Palatine Tonsil']
 # df = df[df['Anatomy'] =='Base of Tongue'] | df[df['Anatomy'] =='Lingual Tonsil'] | df[df['Anatomy'] =='Palatine Tonsil']
@@ -55,11 +58,8 @@ subset_df = subset_df[subset_df['Label'] != 0]
 subset_df['Label'] = subset_df['Label'].replace([1, 2, 18, 20, 25, 27, 29, 36, 37], 0)
 subset_df['Label'] = subset_df['Label'].replace([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 22, 24, 28, 30, 33], 1)
 
-
 # Remove rows where 'Label' is not 0 or 1
 subset_df = subset_df[subset_df['Label'].isin([0, 1])]
-
-
 
 bex_df = optimized_bex(subset_df)
 # bex_df = bex(subset_df)
@@ -74,4 +74,34 @@ selected_columns = df.columns[df.columns.str.contains(pattern)]
 selected_columns = selected_columns.insert(len(selected_columns), 'Label')
 TrainTest_Data = bex_df[selected_columns]
 
-# Here you can filter the data based on case numbrer
+
+# Here you can filter the data based on case number
+
+# ----------------------------SHAP Analysis----------------------------
+
+
+# Open .pkl files
+def load_pickle_file():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(filetypes=[("Pickle files", "*.pkl")])
+
+    if file_path:
+        with open(file_path, 'rb') as file:
+            data = pickle.load(file)
+        root.destroy()
+        return data
+    else:
+        print("No file selected.")
+        root.destroy()
+        return None
+
+
+data = load_pickle_file()
+xgb_model = data['model']
+dataset = subset_df
+
+# SHAP Explainer
+explainer = shap.TreeExplainer(xgb_model)
+shap_values = explainer.shap_values(dataset)
+shap.summary_plot(shap_values, dataset)
